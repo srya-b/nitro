d3f351587bfa5614d2d08e29368e746dba2be20e
Surya Bakshi <surya.bakshi@protonmail.com>
2025-03-26 18:53:41 +0000

    Set up for logging ops and data, and print statements

--- execution/gethexec/executionengine.go
+++ execution/gethexec/executionengine.go
@@ -556,6 +556,7 @@ func (s *ExecutionEngine) sequenceTransactionsWithBlockMutex(header *arbostypes.
 	statedb.StartPrefetcher("Sequencer", witness)
 	defer statedb.StopPrefetcher()
 	delayedMessagesRead := lastBlockHeader.Nonce.Uint64()
+	statedb.StartLogger()
 
 	startTime := time.Now()
 	block, receipts, err := arbos.ProduceBlockAdvanced(
@@ -761,6 +762,8 @@ func (s *ExecutionEngine) createBlockFromNextMessage(msg *arbostypes.MessageWith
 	} else {
 		runCtx = core.NewMessageCommitContext(s.wasmTargets)
 	}
+
+	statedb.StartLogger()
 	block, receipts, err := arbos.ProduceBlock(
 		msg.Message,
 		msg.DelayedMessagesRead,

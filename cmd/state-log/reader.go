e89dc7bef8d8384c05888c86a1b4e59146bfc61b
Surya Bakshi <surya.bakshi@protonmail.com>
2025-07-04 00:16:47 +0000

    log processor and executor

---
+++ cmd/state-log/reader.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+    "math/big"
+
+	"github.com/ethereum/go-ethereum/core/state"
+    "github.com/ethereum/go-ethereum/log"
+)
+
+var logDir string = "/home/sbakshi/arb1/arb-devnode/state-logs"
+
+func readPre(b *big.Int, v int) ([]byte, error) {
+	fn := fmt.Sprintf("%s/predata-%v-%d.json", logDir, b, v)
+	log.Info("Getting Pre", "fn", fn)
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	content, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
+func readPost(b *big.Int, v int) ([]byte, error) {
+	fn := fmt.Sprintf("%s/postdata-%v-%d.json", logDir, b, v)
+	log.Info("Getting Post", "fn", fn)
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	content, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
+func preFromBytes(b []byte) *state.PreLog {
+	var obj state.PreLog
+	err := json.Unmarshal(b, &obj)
+	if err != nil {
+		panic(err)
+	}
+	return &obj
+}
+
+func postFromBytes(b []byte) *state.PostLog {
+	var obj state.PostLog
+	err := json.Unmarshal(b, &obj)
+	if err != nil {
+		panic(err)
+	}
+	return &obj
+}

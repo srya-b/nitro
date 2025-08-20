61f671cba20a208a4e7e10220b06d58ef17302d7
srya-b <surya.bakshi@protonmail.com>
2025-08-20 15:43:20 +0000

    validated the first preObj

--- cmd/state-log/reader.go
+++ cmd/state-log/reader.go
@@ -4,18 +4,155 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/big"
+	_"math/big"
 	"os"
+	"strings"
+	"strconv"
+	"sort"
 
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/log"
 )
 
-var logDir string = "/home/sbakshi/arb1/arb-devnode/state-logs"
+type Prepost int
 
-func readPre(b *big.Int, v int) ([]byte, error) {
+const (
+	PRE Prepost = iota
+	POST
+)
+
+// LogFile represents a log file with its name and the first number for sorting.
+type LogFile struct {
+	FileName string
+	Blockno  int
+	Type     Prepost
+	Count    int
+}
+
+func (l LogFile) String() string {
+	return l.FileName
+}
+
+// ByNum implements the sort.Interface for sorting LogFile slices by the 'Num' field.
+type ByBlock []LogFile
+type ByTotalOrder []LogFile
+
+func (a ByBlock) Len() int           { return len(a) }
+func (a ByBlock) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByBlock) Less(i, j int) bool { return a[i].Blockno < a[j].Blockno }
+
+func (a ByTotalOrder) Len() int		 		{ return len(a) }
+func (a ByTotalOrder) Swap(i, j int)		{ a[i], a[j] = a[j], a[i] }
+func (a ByTotalOrder) Less(i, j int) bool {
+	if a[i].Type == PRE && a[j].Type == PRE {
+		return a[i].Count < a[j].Count
+	} else if a[i].Type == PRE && a[j].Type == POST {
+		// if they have the same count then PRE is earlier than POST
+		if a[i].Count == a[j].Count {
+			return true
+		} else {
+			return a[i].Count < a[j].Count
+		}
+	} else if a[i].Type == POST && a[j].Type == PRE {
+		if a[i].Count == a[j].Count {
+			return false
+		} else {
+			return a[i].Count < a[j].Count
+		}
+	} else if a[i].Type == POST && a[j].Type == POST {
+		return a[i].Count < a[j].Count
+	} else {
+		panic(fmt.Sprintf("unknown type combo: %v, %v", a[i], a[j]))
+	}
+}
+
+func getLogFilesSorted(dir string) [][]LogFile {
+	var logFiles []LogFile
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading dir %v", err))
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		filename := file.Name()
+		// Check if the filename matches the format "string-number-number.json"
+		if strings.HasSuffix(filename, ".json") {
+			parts := strings.Split(strings.TrimSuffix(filename, ".json"), "-")
+			if len(parts) == 3 {
+				var preorpost Prepost
+				if parts[0] == "predata" {
+					preorpost = PRE
+				} else if parts[0] == "postdata" {
+					preorpost = POST
+				} else {
+					panic(fmt.Sprintf("file is neither predata or postdata : %v", parts[0]))
+				}
+
+				blockno, err := strconv.Atoi(parts[1])
+				if err != nil {
+					panic(fmt.Sprintf("Blockno couldn't be read as an int: %v", parts[1]))
+				}
+				iteration, err := strconv.Atoi(parts[2])
+				if err != nil {
+					panic(fmt.Sprintf("Iteration couldn't be read as an int: %v", parts[2]))
+				}
+
+				lf := LogFile{
+					FileName: filename,
+					Blockno: blockno,
+					Type: preorpost,
+					Count: iteration,
+				}
+				logFiles = append(logFiles, lf)
+			}
+		}
+	}
+
+	sort.Sort(ByBlock(logFiles))
+
+	// group them by block number in a 2d array
+	var groupedLogFiles [][]LogFile
+	currBlockno := 0
+		
+	for _, lf := range logFiles {
+		if lf.Blockno != currBlockno {
+			groupedLogFiles = append(groupedLogFiles, []LogFile{})
+			currBlockno = lf.Blockno
+		}
+		groupedLogFiles[len(groupedLogFiles)-1] = append(groupedLogFiles[len(groupedLogFiles)-1], lf)
+	}
+
+	// sanitycheck
+	numsSeen := make(map[int]bool)
+	for _, lfs := range groupedLogFiles {
+		if len(lfs) > 0 {
+			n := lfs[0].Blockno
+			_, ok := numsSeen[n]
+			if ok {
+				panic(fmt.Sprintf("Seen this index already: %v", lfs))
+			}
+			numsSeen[n] = true
+			for _, lf := range lfs {
+				if lf.Blockno != n {
+					panic(fmt.Sprintf("Not grouped correctly: %v", lfs))
+				}		
+			}
+			sort.Sort(ByTotalOrder(lfs))
+		}
+	}
+
+	log.Debug("Grouped log files", "sample", groupedLogFiles[0])
+	return groupedLogFiles
+}
+
+func readPre(b int, v int) ([]byte, error) {
+//func readPre(lf LogFile) ([]byte, error) {
 	fn := fmt.Sprintf("%s/predata-%v-%d.json", logDir, b, v)
-	log.Info("Getting Pre", "fn", fn)
+	log.Info("preLog", "file", fn)
 	f, err := os.Open(fn)
 	if err != nil {
 		return nil, err
@@ -29,8 +166,10 @@ func readPre(b *big.Int, v int) ([]byte, error) {
 	return content, nil
 }
 
-func readPost(b *big.Int, v int) ([]byte, error) {
+func readPost(b  int, v int) ([]byte, error) {
+//func readPost(lf LogFile) ([]byte, error) {
 	fn := fmt.Sprintf("%s/postdata-%v-%d.json", logDir, b, v)
+	//fn := lf.FileName
 	log.Info("Getting Post", "fn", fn)
 	f, err := os.Open(fn)
 	if err != nil {
@@ -62,3 +201,41 @@ func postFromBytes(b []byte) *state.PostLog {
 	}
 	return &obj
 }
+
+//func getPreObj(blockno *big.Int, version int) (*state.PreLog, bool) {
+func getPreObj(blockno int, version int) (*state.PreLog, bool) {
+	content, err := readPre(blockno, version)
+	if err != nil {
+		log.Error("No pre file", "e", err, "version", version)
+		return nil, false
+	}
+	preObj := preFromBytes(content)
+	// samplePreData(preObj)
+	return preObj, true
+}
+
+func getPostObj(blockno int, version int) (*state.PostLog, bool) {
+	content, err := readPost(blockno, version)
+	if err != nil {
+		log.Error("No post file", "err", err, "version", version)
+		return nil, false
+	}
+	postObj := postFromBytes(content)
+	// samplePostData(postObj)
+	return postObj, true
+}
+
+func getPrePostObjs(blockno int, version int) (*state.PreLog, *state.PostLog, bool) {
+	preObj, exists := getPreObj(blockno, version)
+	if !exists {
+		return nil, nil, false
+	}
+
+	postObj, exists := getPostObj(blockno, version)
+	if !exists {
+		log.Error("Prelog exists and postLog doesn't", "blockno", blockno, "version", version)
+		panic("Missing postLog")
+	}
+	return preObj, postObj, true
+}
+
--- cmd/state-log/tests.go
+++ cmd/state-log/tests.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"math/big"
+	"fmt"
+	_"math/big"
 	"reflect"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -9,28 +10,47 @@ import (
 	_ "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/trie"
-	_ "github.com/ethereum/go-ethereum/trie"
 )
 
-func CheckPostDataLogsEverything() {
-	blockno := big.NewInt(0)
+func CheckPostDataLogsEverything(dir string) {
+	logFiles := getLogFilesSorted(dir)
+	log.Info("Number of logs", "n", len(logFiles))
 
-	preObj, exists := getPreObj(blockno, 1)
+	//blockno := big.NewInt(0)
+
+	preObj, exists := getPreObj(logFiles[0][0].Blockno, 1)
 	if !exists {
 		log.Error("FIrst prelog should exists")
 		panic("Read preloog error")
 	}
 
-	postObj, exists := getPostObj(blockno, 1)
+	_, exists = getPostObj(logFiles[0][0].Blockno, 1)
 	if !exists {
 		log.Error("Last post log version 2 should be there")
 		panic("Read post log error")
 	}
 
 	validatePreLog(preObj)
-	validatePostLog(postObj)
+	//validatePostLog(postObj)
 
-	ExploreTrieKeys(preObj)
+	//ExploreTrieKeys(preObj)
+}
+
+func checkCreatesDeletes(preObj *state.PreLog) (state.Set, state.Set, state.Set) {
+	createdAccounts := state.GetCreatedAccounts(preObj.Journals)
+	deletedAccounts := state.GetDeletedAccounts(preObj.Journals)
+	emptyDeletes := state.GetEmptyDeletes(preObj.EmptyDeletes, preObj.Journals)
+	// sanity check our data
+	for _, l := range preObj.CreateDelete {
+		for _, addr := range l {
+			_, created := createdAccounts[addr]
+			_, deleted := deletedAccounts[addr]
+			if created || deleted {
+				panic(fmt.Sprintf("Created (%v) or Deleted(%v) returned an account that was createdAndDeleted: %v", created, deleted, addr))
+			}
+		}
+	}
+	return mapSubtract(createdAccounts, emptyDeletes), MergeMaps(deletedAccounts, emptyDeletes), emptyDeletes
 }
 
 func validatePreLog(preObj *state.PreLog) bool {
@@ -41,33 +61,28 @@ func validatePreLog(preObj *state.PreLog) bool {
 		return true
 	}
 
-	// preKeys, _ := ExploreTrie(preObj)
-	// accountsFound := listToSet(preKeys)
+	preAccounts, _ := ExploreTrie(preObj)
+	//preTrie := ValidatorTrieFromObj(preObj)
+	hAccountsReached := listToSet(preAccounts)
 
-	// all the accounts that were created (unhashed) from the Journal
-	createdAccounts := state.GetCreatedAccounts(preObj.Journals)
-	deletedAccounts := state.GetDeletedAccounts(preObj.Journals)
-	emptyDeletes := GetEmptys(preObj)
-	realAccountDeletes := MergeMaps(deletedAccounts, emptyDeletes)
+	// all the accounts that were created in this set and persist to the
+	// future, all other created and deleted will not be returned here
+	// some created accounts may be deleted because of being empty
+	// and that won't be captured by reverts or self destructs
+	createdAccounts, _, emptyDeletes := checkCreatesDeletes(preObj)
+	hCreatedAccounts := hashAddressSet(createdAccounts)
+	_ = hCreatedAccounts
+	hEmptyDeletes := hashAddressSet(emptyDeletes)
+	_ = hEmptyDeletes
 
-	createdKeys := state.GetCreatedKeys(preObj.Journals)
-	zeroGets := state.GetKeysAlwaysZero(preObj.Journals)
-	deletedKeys := state.GetDeletedKeys(preObj.Journals)
-
-	// the accounts in the trie in objects
-	// accountsInTrie := make(map[common.Hash]bool)
-	accountsInTrie := hashAddressSet(mapSubtract(mapToSet(preObj.Accounts), createdAccounts))
-	keysInTrie := mapSubtract(mapToSet(preObj.Keys), createdKeys)
-	keysInTrie = mapSubtract(keysInTrie, zeroGets)
-
-	log.Info("Accounts in Trie", "map", accountsInTrie)
-	log.Info("Created accounts", "map", hashAddressSet(createdAccounts))
-	log.Info("Deleted accounts", "map", hashAddressSet(realAccountDeletes))
-
-	log.Info("Keys in trie", "map", hashKeySet(keysInTrie))
-	log.Info("Created keys", "map", hashKeySet(createdKeys))
-	log.Info("Deleted keys", "map", hashKeySet(deletedKeys))
-	log.Info("Always 0 keys", "map", hashKeySet(zeroGets))
+	// The accounts that should already be in the trie according to this journal.
+	// Created accounts aren't in the trie yet, and neither are the accounts that were
+	// created and deleted because they were empty. We expect to find exactly these
+	// same accounts when creating a trie with the stored nodes.
+	hAccountsInObj := hashAddressSet(mapToSet(preObj.Accounts))
+	_ = hAccountsInObj
+	existingAccounts := mapSubtract(mapSubtract(mapToSet(preObj.Accounts), createdAccounts), emptyDeletes)
+	hExistingAccounts := hashAddressSet(existingAccounts)
 
 	// t := ValidatorTrieFromObj(preObj)
 	// ha := common.HexToHash("0xb4d14ec89c201c23aa60e231e3993b3966b33ff1f55d198ec25980957ab32065")
@@ -75,32 +90,176 @@ func validatePreLog(preObj *state.PreLog) bool {
 	//res := t.GetStateWithHashKey(ha.Bytes(), hk.Bytes())
 	//log.Info("GetState result", "res", res)
 
-	a := common.HexToHash("0xb4d14ec89c201c23aa60e231e3993b3966b33ff1f55d198ec25980957ab32065")
-	k := common.HexToHash("0x299e3e924ceba6fe53dd5651a14c88eb9f6fb9e6c9763369a608e3310fe5709a")
+	//a := common.HexToHash("0xb4d14ec89c201c23aa60e231e3993b3966b33ff1f55d198ec25980957ab32065")
+	//k := common.HexToHash("0x299e3e924ceba6fe53dd5651a14c88eb9f6fb9e6c9763369a608e3310fe5709a")
 
-	for key := range preObj.Keys {
-		hk := common.BytesToHash(trie.PublicHashKey(key.Key().Bytes()))
-		// ha := common.BytesToHash(trie.PublicHashKey(key.Addr().Bytes()))
-		if hk.Cmp(k) == 0 {
-			log.Info("Search Address", "hash", a, "addr", key.Addr())
-			log.Info("Search Key", "hash", hk, "key", key.Key())
-		}
-	}
+	//for key := range preObj.Keys {
+	//	hk := common.BytesToHash(trie.PublicHashKey(key.Key().Bytes()))
+	//	// ha := common.BytesToHash(trie.PublicHashKey(key.Addr().Bytes()))
+	//	if hk.Cmp(k) == 0 {
+	//		log.Info("Search Address", "hash", a, "addr", key.Addr())
+	//		log.Info("Search Key", "hash", hk, "key", key.Key())
+	//	}
+	//}
 
 	// log.Info("Find all")
 	// state.PublicFindAll(a, preObj.Journals)
 	// state.PublicFindGetSets(a, k, preObj.Journals)
 	//log.Info("Return")
 
-	// eq := reflect.DeepEqual(accountsFound, accountsInTrie)
-	// if eq {
-	//	log.Info("Equal found and in journal")
-	//	return true
-	// } else {
-	//	log.Error("Differing", "accountsFound", accountsFound, "accountsInTrie", accountsInTrie)
-	//	return false
-	//}
+	eq := reflect.DeepEqual(hAccountsReached, hExistingAccounts)
+	if eq {
+		log.Info("Equal found and in journal")
+	} else {
+		//log.Error("Differing", "hAccountsReached", hAccountsReached, "hExistingAccounts", hExistingAccounts)
+		log.Error("Found - in trie", "m", mapSubtract(hAccountsReached, hExistingAccounts))
+		log.Error("in trie - Found", "m", mapSubtract(hExistingAccounts, hAccountsReached))
+	}
+
+	hKeysInObj, hKeysInObjPreimages := hashKeySet(mapToSet(preObj.Keys))
+	_, _ = hKeysInObj, hKeysInObjPreimages
+	createdKeys := state.GetCreatedKeys(preObj.Journals)
+	hCreatedKeys, hCreatedKeysPreimages := hashKeySet(createdKeys)
+	_, _ = hCreatedKeys, hCreatedKeysPreimages
+	zeroGets := state.GetKeysAlwaysZero(preObj.Journals)
+	hZeroGets, hZeroGetsPreimages := hashKeySet(zeroGets)
+	_, _ = hZeroGets, hZeroGetsPreimages
+	//deletedKeys := state.GetDeletedKeys(preObj.Journals)
+	keysInTrie := mapSubtract(mapToSet(preObj.Keys), createdKeys)
+	keysInTrie = mapSubtract(keysInTrie, zeroGets)
+
+	// KeyKey from the logs have an address and an unhashed key, but the
+	// trie search will always return the hashed address and the hashed key.
+	// Therefore we need to hash the keys from the log to be able to compare 
+	// it to what is found by the trie.
+	hashedKeysInTrie := make(map[state.HashedKeyKey]bool)
+	keyPreimages := make(map[common.Hash]common.Hash)
+	keyHashes := make(map[common.Hash]common.Hash)
+	for k := range keysInTrie {
+		ha := common.BytesToHash(trie.PublicHashKey(k.Addr().Bytes()))
+		hk := common.BytesToHash(trie.PublicHashKey(k.Key().Bytes()))
+		hkk := state.NewHashedKeyKey(ha, hk)
+		hashedKeysInTrie[hkk] = keysInTrie[k]
+		keyPreimages[hk] = k.Key()
+		keyHashes[k.Key()] = hk
+	}
+
+	preKeys, _ := ExploreTrieKeys(preObj)
+	// make this into a HashedKeyKey map as well
+	hashedKeysFound := make(map[state.HashedKeyKey]bool)
+	for ha, hks := range preKeys {
+		for _, hk := range hks {
+			hkk := state.NewHashedKeyKey(ha, hk)
+			hashedKeysFound[hkk] = true
+		}
+	}
+
+	keyEq := reflect.DeepEqual(hashedKeysFound, hashedKeysInTrie)
+	if keyEq {
+		log.Info("Keys are equal")
+	} else {
+		intrie_found := mapSubtract(hashedKeysInTrie, hashedKeysFound)
+		found_intrie := mapSubtract(hashedKeysFound, hashedKeysInTrie)
+		log.Error("keys not equal", "keysInTrie", len(hashedKeysInTrie), "keysFound", len(hashedKeysFound))
+		log.Error("Difference", "inTrie - found", len(intrie_found), "found - in trie", len(found_intrie))
+
+		// what addresses have the keys not in the other
+		preimageMap, _ := hkkPreimages(keysInTrie, hashedKeysInTrie)
+		// the addresses of the keys
+
+		different := make(map[common.Address]bool)
+		diffNoPreimage := make(map[common.Hash]bool)
+		_ = diffNoPreimage
+		if len(intrie_found) > 0 && len(found_intrie) == 0 {
+			// get the preimages and list which ones aren't in the other
+			for ha := range intrie_found {
+				a, ok := preimageMap[ha.HashAddr()]
+				if ok {
+					different[a] = true
+				}
+			}
+			log.Debug("In journal but not in found", "addrs", different)
+		} 
+
+///// The code below is for diagnosing the extra keys that are found in the trie, but 
+///// aren't in the journal. We figured out that the extra keys present is a case of a
+///// Get request in the journal to a key with a matching prefix that traversed all the 
+///// way down the trie, to the short node{valueNode}, and returned nil but logged the 
+///// leaf as well. If you print the zeroGets for the accounts and the keys not found in the
+///// journal, you notice the missing keys all have a decent shared prefix with the zero gets.
+		//else if len(found_intrie) > 0 && len(intrie_found) == 0 {
+		//	for ha := range found_intrie {
+		//		a, ok := preimageMap[ha.HashAddr()]
+		//		if ok {
+		//			different[a] = true
+		//		} else {
+		//			diffNoPreimage[ha.HashAddr()] = true
+		//		}
+		//	}
+		//	log.Debug("found but not in journal", "addrs", different, "no preimage", len(diffNoPreimage))
+		//	for hd := range diffNoPreimage {
+		//		c := state.PublicFindAllHashKeyCount(hd, preObj.Journals)
+		//		log.Debug("no preimage", "addr", hd, "count", c)
+		//	}
+
+		//	targetha := common.HexToHash("0xb4d14ec89c201c23aa60e231e3993b3966b33ff1f55d198ec25980957ab32065")
+		//	targeta := common.HexToAddress("0xA4b05FffffFffFFFFfFFfffFfffFFfffFfFfFFFf")
+
+		//	// the keys of this found in journal and only in trie
+		//	cInJournal := countHKKInSet(targetha, hashedKeysInTrie)
+		//	cInTrie := countHKKInSet(targetha, found_intrie)
+		//	cZeroGets := countHKKInSet(targetha, hZeroGets)
+
+		//	log.Debug("Target address", "addr", targeta, "cInJournal", len(cInJournal), "cInTrie", len(cInTrie), "zeroGets", len(cZeroGets))
+
+		//	log.Debug("cInJournal")
+		//	for _, inj := range cInJournal {
+		//		log.Debug("Hashed key", "hk", inj)
+		//		state.PublicFindAllHashKeyKey(inj, preObj.Journals)
+		//	}
+		//	log.Debug("found_intrie")
+		//	for _, inj := range cInTrie {
+		//		log.Debug("Hashed key", "hk", inj)
+		//		state.PublicFindAllHashKeyKey(inj, preObj.Journals)
+		//	}
+
+		//	for _, inj := range cZeroGets {
+		//		log.Debug("Zero get", "hk", inj)
+		//	}
+
+
+		//	_, hkInObj := hKeysInObj[targethkk]
+		//	_, hkInCreated := hKeysInObj[targethkk]
+		//	log.Debug("Target membership", "inObj", hkInObj, "inCreated", hkInCreated)
+		//	state.PublicFindAllHashKey(targethkk.HashAddr(), preObj.Journals)
+
+		//	 inspect one element of the nopreimage set
+		//	k, _ := arbMapElement(found_intrie)
+		//	log.Debug("Arb element", "k", k)
+		//	state.PublicFindAllHashKeyKey(k, preObj.Journals)
+		//	log.Debug("Account actions", "ha", k.HashAddr())
+		//	state.PublicFindAllHashKeyStorage(k.HashAddr(), preObj.Journals)
+
+		//	//for d := range different {
+		//	//	log.Debug("Difference", "addr", d, 
+		//	//	state.PublicFindAll(d, preObj.Journals)
+		//	//}
+		//} else {
+		//	panic(fmt.Sprintf("Both have exclusive elements"))
+		//}
+/////
+	}
+
 	return true
+}
+
+func arbMapElement[K comparable, V any](m map[K]V) (K, V) {
+	var zeroK K
+	var zeroV V
+	for k, v := range m {
+		return k, v
+	}
+	return zeroK, zeroV
 }
 
 func validatePostLog(postObj *state.PostLog) bool {
